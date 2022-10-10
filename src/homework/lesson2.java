193b4aea699ae4ad188fa0dd5543b3fa29727c4b
Mike Teslin <3131868@gmail.com>
2022-10-10 18:39:40 +0300

    add homework lesson 2

---
+++ src/homework/lesson2.java
@@ -0,0 +1,74 @@
+package homework;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class lesson2 {
+    public static void main(String[] args) {
+        byte b1 = 12;
+        byte b2 = 0xc;
+        byte b3 = 014;
+        byte b4 = 0b1100;
+
+        System.out.print(b1);
+        System.out.print(b2);
+        System.out.print(b3);
+        System.out.println(b4);
+
+        short s1 = 1300;
+        short s2 = 0x514;
+        short s3 = 02424;
+        short s4 = 0b010100010100;
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
+        int i1 = 0;
+        int i2 = 0x0;
+        int i3 = 00;
+        int i4 = 0b0;
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+
+        long l1 = 123456789;
+        long l2 = 0x75BCD15;
+        long l3 = 0726746425;
+        long l4 = 0b111010110111100110100010101;
+
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+
+        double d1 = 0.2;
+        double d2 = 0.3;
+
+        float f1 = 4.5f;
+        float f2 = 5.6f;
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(bool1);
+        System.out.println(bool2);
+
+        char c1 = 'a';
+        char c2 = '\u1234';
+        char c3 = 333;
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+    }
+}
--- src/lesson2/PrimitiveDataTypes.java
+++ src/lesson2/PrimitiveDataTypes.java
@@ -12,5 +12,36 @@ public class PrimitiveDataTypes {
 
         int i1 = 500;
 
+        long l1 = 60000;
+        long l2 = 1000000000;
+
+        float f1 = 2.3f;
+
+        double d1 = 5.4D;
+        double d2 = 67.5d;
+
+        char c1 = 'a';
+        char c2 = 'A';
+        char c3 = '7';
+
+        char c4 = 300;
+        char c5 = 500; // 10
+
+        char c6 = '\u1234'; // 16
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        System.out.println("privet");
+        System.out.println(s1 + s2);
+        System.out.println(d1 / d2);
+        System.out.println(c4);
+        System.out.println(c5);
+        System.out.println(c6);
+        System.out.println(bool1);
+        System.out.println(bool2);
+
+
+
     }
 }
