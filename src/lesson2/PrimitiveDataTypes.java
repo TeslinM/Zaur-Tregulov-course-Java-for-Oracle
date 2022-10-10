95997d765edea8832bc09f1f4859fcc922a415a8
Mike Teslin <3131868@gmail.com>
2022-10-10 17:12:12 +0300

    lesson 2

---
+++ src/lesson2/PrimitiveDataTypes.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte b1 = 10;
+        byte b2 = 20;
+        int b3 = 1000;
+
+        short s1 = 5;
+        short s2 = -10;
+        short s3 = 0;
+
+        int i1 = 500;
+
+    }
+}
